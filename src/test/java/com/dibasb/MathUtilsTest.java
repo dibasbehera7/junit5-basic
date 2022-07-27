94259591a5ae15b0241898b581ad3328852fbca0
dibasbehera7 <dibasbehera@gmail.com>
2022-07-27 15:28:47 +0530

    Added CircleArea Computation

--- src/test/java/com/dibasb/MathUtilsTest.java
+++ src/test/java/com/dibasb/MathUtilsTest.java
@@ -7,12 +7,19 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 
 		MathUtils mathUtils = new MathUtils();
-		int expected = 1;
+		int expected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual, "The add method should add two numbers");
+	}
+
+	@Test
+	void testComputeCircleRadius() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.159265358979323846, mathUtils.computeCircleArea(10), "Should return right circle area");
+
 	}
 
 }
