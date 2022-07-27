c5c6cb421a9b15408ebd09f94933d725047be644
dibasbehera7 <dibasbehera@gmail.com>
2022-07-27 15:19:07 +0530

    Added failed test case

--- src/test/java/com/dibasb/MathUtilsTest.java
+++ src/test/java/com/dibasb/MathUtilsTest.java
@@ -10,9 +10,9 @@ class MathUtilsTest {
 	void test() {
 
 		MathUtils mathUtils = new MathUtils();
-		int expected = 2;
+		int expected = 1;
 		int actual = mathUtils.add(1, 1);
-		assertEquals(expected, actual);
+		assertEquals(expected, actual, "The add method should add two numbers");
 	}
 
 }
