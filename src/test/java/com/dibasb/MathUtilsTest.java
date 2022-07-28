949b63bb374dd5104636982187ff871a4288e2b7
dibasbehera7 <dibasbehera@gmail.com>
2022-07-28 12:19:07 +0530

    BeforeEach annotation added

--- src/test/java/com/dibasb/MathUtilsTest.java
+++ src/test/java/com/dibasb/MathUtilsTest.java
@@ -1,23 +1,35 @@
 package com.dibasb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	MathUtils mathUtils;
+
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+
 	@Test
 	void testAdd() {
 
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual, "The add method should add two numbers");
 	}
 
 	@Test
+	void testDivide() {
+
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
+	}
+
+	@Test
 	void testComputeCircleRadius() {
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.159265358979323846, mathUtils.computeCircleArea(10), "Should return right circle area");
 
 	}
